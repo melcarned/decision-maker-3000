b71669d1e69cb980b6f0e738589b18b41c6fd39c
melcarned <melcarned@gmail.com>
2017-11-04 13:45:58 -0400

    returns random number

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = Math.floor(Math.random() * 100);
 
   options = {
     hostname: 'api.groupme.com',
