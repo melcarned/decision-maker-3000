414f713f3e189ece37fa953b7d3211d2c6f94eb2
melcarned <melcarned@gmail.com>
2017-11-04 13:51:39 -0400

    string

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = Math.floor(Math.random() * 100);
+  botResponse = "->" + Math.floor(Math.random() * 100);
 
   options = {
     hostname: 'api.groupme.com',
