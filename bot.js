c6bf14a0c2019371d7e43d3064110bd3fc990b0e
melcarned <melcarned@gmail.com>
2017-11-04 15:52:18 -0400

    added random image

--- bot.js
+++ bot.js
@@ -2,10 +2,19 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
-var images = {
+var images = [
+  "https://i.groupme.com/1200x676.jpeg.88c57afb3454485aa8156f2d3e871261",
+  "https://i.groupme.com/529x433.jpeg.9292a98cd15e4955923de1c564b391d7",
+  "https://i.groupme.com/450x299.jpeg.e3f75580c4f74edbbf723b30689ab172",
+  "https://i.groupme.com/1000x667.jpeg.81b0b68fe4244149989253d6b03ba634",
+  "https://i.groupme.com/759x422.jpeg.8daf773eeadd4ed98959e30301faa87f",
+  "https://i.groupme.com/1300x957.jpeg.20aa0c3490024a0ea69980ac4f30485e",
+  "https://i.groupme.com/360x240.jpeg.ce1fc0f866ef45cab7acb38b9315259b",
+  "https://i.groupme.com/1536x826.jpeg.e35e46a939e7464e8bb2aa991fcca01b",
+  "https://i.groupme.com/640x334.jpeg.875b0386de6e4ee9939d6793cf052db8",
+  "https://i.groupme.com/1000x664.jpeg.3a3d11123004463fa1bfd946fecb1124"
+];
 
-
-};
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
@@ -43,13 +52,13 @@ function postMessage(randomNumber) {
 
   body = {
     "bot_id" : botID,
-    "text" : botResponse,
     "attachments" : [
       {
         "type"  : "image",
-        "url"   : "https://i.groupme.com/1200x676.jpeg.88c57afb3454485aa8156f2d3e871261"
+        "url"   : images[Math.floor(Math.random() * 9)]
       }
-    ]
+    ],
+    "text" : botResponse
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
