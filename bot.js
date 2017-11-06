c82cb2038601756c7016a03cef52c21a0e73fa3b
melcarned <melcarned@gmail.com>
2017-11-05 21:33:32 -0500

    Added 11 more images

--- bot.js
+++ bot.js
@@ -1,21 +1,10 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
-
+var images = require('images');
 var botID = process.env.BOT_ID;
 var undecidedMessage = "I'm not sure what to decide on. Please ask me again by saying \"@DM3K Should I [insert your action here]?\"";
 
-var images = [
-  "https://i.groupme.com/1200x676.jpeg.88c57afb3454485aa8156f2d3e871261",
-  "https://i.groupme.com/529x433.jpeg.9292a98cd15e4955923de1c564b391d7",
-  "https://i.groupme.com/450x299.jpeg.e3f75580c4f74edbbf723b30689ab172",
-  "https://i.groupme.com/1000x667.jpeg.81b0b68fe4244149989253d6b03ba634",
-  "https://i.groupme.com/759x422.jpeg.8daf773eeadd4ed98959e30301faa87f",
-  "https://i.groupme.com/1300x957.jpeg.20aa0c3490024a0ea69980ac4f30485e",
-  "https://i.groupme.com/360x240.jpeg.ce1fc0f866ef45cab7acb38b9315259b",
-  "https://i.groupme.com/1536x826.jpeg.e35e46a939e7464e8bb2aa991fcca01b",
-  "https://i.groupme.com/640x334.jpeg.875b0386de6e4ee9939d6793cf052db8",
-  "https://i.groupme.com/1000x664.jpeg.3a3d11123004463fa1bfd946fecb1124"
-];
+
 
 
 function respond() {
---
+++ images-lib.js
@@ -0,0 +1,23 @@
+var images = [
+  "https://i.groupme.com/1200x676.jpeg.88c57afb3454485aa8156f2d3e871261",
+  "https://i.groupme.com/529x433.jpeg.9292a98cd15e4955923de1c564b391d7",
+  "https://i.groupme.com/450x299.jpeg.e3f75580c4f74edbbf723b30689ab172",
+  "https://i.groupme.com/1000x667.jpeg.81b0b68fe4244149989253d6b03ba634",
+  "https://i.groupme.com/759x422.jpeg.8daf773eeadd4ed98959e30301faa87f",
+  "https://i.groupme.com/1300x957.jpeg.20aa0c3490024a0ea69980ac4f30485e",
+  "https://i.groupme.com/360x240.jpeg.ce1fc0f866ef45cab7acb38b9315259b",
+  "https://i.groupme.com/1536x826.jpeg.e35e46a939e7464e8bb2aa991fcca01b",
+  "https://i.groupme.com/640x334.jpeg.875b0386de6e4ee9939d6793cf052db8",
+  "https://i.groupme.com/1000x664.jpeg.3a3d11123004463fa1bfd946fecb1124",
+  "https://i.groupme.com/400x267.jpeg.6ae1d221c8794d8c97524e183b86fb4c",
+  "https://i.groupme.com/900x565.jpeg.301043e0668249d88701ddfa6fd6e94b",
+  "https://i.groupme.com/600x400.jpeg.c683bda5a6de4d4ebacd3f8446a66fd3",
+  "https://i.groupme.com/1300x866.jpeg.c261e421ac20494e8544b3caa62adb1d",
+  "https://i.groupme.com/450x320.jpeg.077500887b8241a4a8f1ea4b860e7574",
+  "https://i.groupme.com/400x200.jpeg.8e2ab5560979476a8b7769e169c395ae",
+  "https://i.groupme.com/323x520.jpeg.a519617958e34de1ba1451219c2e3bbc",
+  "https://i.groupme.com/425x282.jpeg.6087b75fa85a45848f33cc13e9086015",
+  "https://i.groupme.com/292x247.jpeg.7f0fe1bc48b84cc9b8ce1860f259c67a",
+  "https://i.groupme.com/1500x1446.jpeg.dc30bd1068bc4f56a2eb59f420d04ca5",
+  "https://i.groupme.com/680x473.jpeg.887ba2c742044edf86ea39fe71cb70d7"
+];
