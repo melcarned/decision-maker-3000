1917545a731334a34b15db31f054c1e784cafcea
melcarned <melcarned@gmail.com>
2017-11-20 18:01:30 -0500

    last changes

--- index.js
+++ index.js
@@ -9,6 +9,41 @@ bot         = require('./bot.js');
 bot.respond();
 */
 
+var MTA_ID = process.env.MTA_API_KEY;
+
+var Mta = require('mta-gtfs');
+var mta = new Mta({
+  key: '8e0980acaacf6c95b01eec5b261e1dc7', // only needed for mta.schedule() method
+  feed_id: 1                  // optional, default = 1
+});
+
+mta.schedule(200).then(function (result) {
+  console.log(result);
+});
+
+
+/*
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+
+var requestSettings = {
+  method: 'GET',
+  url: 'http://datamine.mta.info/mta_esi.php?key=8e0980acaacf6c95b01eec5b261e1dc7',
+  encoding: null
+};
+
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
+*/
+/*
 router = new director.http.Router({
   '/' : {
     post: bot.respond,
@@ -35,3 +70,4 @@ function ping() {
   this.res.writeHead(200);
   this.res.end("hello mate " + (Math.floor(Math.random() * 100) + 1));
 }
+*/
